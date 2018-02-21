2e7fc9035e7bde886051a4e9c230772a9d70eafb
andrew1007 <yueh.andrew@gmail.com>
2018-02-20 19:24:02 -0800

    fixed cart bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def current_user
-    if session[:session_token]
-      @current_user = User.find_by_session_token(session[:session_token])
+    user = User.find_by_session_token(session[:session_token])
+    if user
+      @current_user = user
     else
       user = User.new
       session_token = SecureRandom.urlsafe_base64(16)
